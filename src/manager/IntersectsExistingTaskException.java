9d86ba03a2ce8c22a6645c06240683ea1072f2c2
Maxim <fixor@yandex.ru>
2024-07-01 18:26:02 +0300

    checkstyle fix

--- src/manager/IntersectsExistingTaskException.java
+++ src/manager/IntersectsExistingTaskException.java
@@ -1,6 +1,6 @@
 package manager;
 
-public class IntersectsExistingTaskException extends RuntimeException{
+public class IntersectsExistingTaskException extends RuntimeException {
     public IntersectsExistingTaskException(String message) {
         super(message);
     }
