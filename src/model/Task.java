7bf2f5fe6f2ac1f4b8255279f33745000d177364
Maxim <fixor@yandex.ru>
2024-07-02 19:34:57 +0300

    checkstyle fix

--- src/model/Task.java
+++ src/model/Task.java
@@ -77,7 +77,7 @@ public class Task {
     }
 
     public LocalDateTime getEndTime() {
-        if (startTime == null){
+        if (startTime == null) {
             return null;
         }
         return startTime.plus(duration);
